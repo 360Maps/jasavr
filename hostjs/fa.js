099f22af6362e8287025f49c9325db7b4cd8eef9
360Maps <157185371+360Maps@users.noreply.github.com>
2024-02-28 21:10:08 +0700

    Add files via upload

---
+++ hostjs/fa.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+{
+   ShowObjectWithEffect('wb_artikel1', 0, 'fade', 1);
+   ShowObjectWithEffect('wb_artikel2', 0, 'fade', 1);
+   ShowObjectWithEffect('wb_artikel3', 0, 'fade', 1);
+   ShowObjectWithEffect('wb_artikel4', 0, 'fade', 1);
+});
